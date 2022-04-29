41c60b4a89336bcea3652c7530063d1eec37bd0f
emre <emre@DESKTOP-IO583UC>
2022-04-29 17:05:57 +0300

    Add project files.

---
+++ src/api/infrastructure/BlazorSozluk.Infrastructure.Persistence/EntityConfigurations/EntryComment/EntryCommentFavoriteEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.EntityConfigurations.EntryComment
+{
+    public class EntryCommentFavoriteEntityConfiguration : BaseEntityConfiguration<Api.Domain.Models.EntryCommentFavorite>
+    {
+        public override void Configure(EntityTypeBuilder<EntryCommentFavorite> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("entrycommentfavorite", BlazorSozlukContext.DEFAULT_SCHEMA);
+
+            builder.HasOne(x => x.EntryComment)
+                .WithMany(x => x.EntryCommentFavorites)
+                .HasForeignKey(x => x.EntryCommentId);
+
+            builder.HasOne(x => x.CreatedUser)
+                .WithMany(x => x.EntryCommentFavorites)
+                .HasForeignKey(x => x.CreatedById);
+        }
+    }
+}
