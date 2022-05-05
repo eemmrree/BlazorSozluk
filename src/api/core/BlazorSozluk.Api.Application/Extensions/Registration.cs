55ff852e8841e8183f2b74fe8574dadb119bd5ff
Emre Bayrak <emrebayrak10@gmail.com>
2022-05-05 11:14:30 +0300

    automapper added for user login created token for user login user controller added create validation for user login

---
+++ src/api/core/BlazorSozluk.Api.Application/Extensions/Registration.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddApplicationRegistration(this IServiceCollection services)
+        {
+            var assem = Assembly.GetExecutingAssembly();
+            services.AddMediatR(assem);
+            services.AddAutoMapper(assem);
+            services.AddValidatorsFromAssembly(assem);
+            return services;
+        }
+    }
+}
---
+++ src/api/webapi/BlazorSozluk.Api.WebApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSozluk.Api.WebApi.Controllers;
+    
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public UserController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    [Route("Login")]
+    public async Task<IActionResult> Login([FromBody]LoginUserCommand loginUser)
+    {
+        var res = await mediator.Send(loginUser);
+        return Ok(res);
+    }
+}
+
